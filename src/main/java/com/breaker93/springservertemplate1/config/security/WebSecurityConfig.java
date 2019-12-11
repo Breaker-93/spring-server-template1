36041ca286ecaeb058730ff61a7098c0ae18f79d
Breaker-93 <1094143240@qq.com>
2019-12-11 17:24:44 +0800

    Merge branch 'master' of https://github.com/Breaker-93/spring-server-template1
    # Conflicts:
    #	src/main/java/com/breaker93/springservertemplate1/config/security/WebSecurityConfig.java
    #	src/main/resources/application-dev.yml

--- src/main/java/com/breaker93/springservertemplate1/config/security/WebSecurityConfig.java
+++ src/main/java/com/breaker93/springservertemplate1/config/security/WebSecurityConfig.java
@@ -46,7 +46,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .formLogin() // 被拦截的请求会自动跳转至默认的登录页面
                 .and()
-                .exceptionHandling().accessDeniedHandler(getAccessDeniedHandler())
+                .exceptionHandling()
+                .authenticationEntryPoint(new RestAuthenticationEntryPoint())
                 .and()
                 .addFilterBefore(getJwtLoginFilter(), UsernamePasswordAuthenticationFilter.class)
                 .addFilterBefore(getJwtFilter(),UsernamePasswordAuthenticationFilter.class)
